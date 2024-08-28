71299e410bb598dc8dc5bed65488b8fa0a07d687
Atsushi Sasaki <atsushi.sasaki@aselectroworks.com>
2024-08-28 23:44:36 +0900

    add blink example

--- examples/blink/blink.ino
+++ examples/blink/blink.ino
@@ -1,11 +1,47 @@
 #include "NeoPixel_SPI.h"
 
-void setup()
-{
-	
+#define SPI_NSS_PIN PB12
+#define SPI_SCK_PIN PB13
+#define SPI_MISO_PIN PB14
+#define SPI_MOSI_PIN PB15
+
+#define NUM_LEDS 1
+
+// SPI Controls
+SPIClass spiLed(SPI_MOSI_PIN, SPI_MISO_PIN, SPI_SCK_PIN);
+SPISettings spiSettings = SPISettings(1500000, MSBFIRST, SPI_MODE2, SPI_TRANSMITONLY);
+NeoPixel_SPI neopixel(NUM_LEDS);
+
+void setup() {
+    // SPI Configure fro Neopixel
+    SPI.setMOSI(SPI_MOSI_PIN);
+    SPI.setMISO(SPI_MISO_PIN);
+    SPI.setSCLK(SPI_SCK_PIN);
+    // SPI.setSSEL(SPI_NSS_PIN);
+    digitalWrite(MOSI, HIGH);
+    delay(1);
+    SPI.begin();
+    SPI.beginTransaction(spiSettings);
+    neopixel.begin(&SPI);    
 }
 
-void loop()
-{
-	
+void loop() {
+    neopixel.setPixel(0, 0, 0, 0);
+    digitalWrite(MOSI, LOW);
+    delay(1);
+    neopixel.show();
+    digitalWrite(MOSI, HIGH);
+    delay(1);
+
+    delay(1000); 
+    
+    neopixel.setPixel(0, 63, 0, 0);
+    digitalWrite(MOSI, LOW);
+    delay(1);
+    neopixel.show();
+    digitalWrite(MOSI, HIGH);
+    delay(1);
+
+    delay(1000); 
+
 }
--- src/NeoPixel_SPI.cpp
+++ src/NeoPixel_SPI.cpp
@@ -1,5 +1,4 @@
 #include "NeoPixel_SPI.h"
-#include "USBSerial_CH32.h"
 
 NeoPixel_SPI::NeoPixel_SPI(uint8_t ledNum) {
     _ledNum = ledNum;
@@ -16,13 +15,10 @@ void NeoPixel_SPI::begin(SPIClass* spi) {
 void NeoPixel_SPI::end() {}
 
 bool NeoPixel_SPI::setPixel(int8_t idx, uint8_t r, uint8_t g, uint8_t b) {
-    // SerialUSB.printf("SET IDX: %2d, R: %3d, G: %3d, B: %3d\r\n", idx, r, g, b);
-    delay(1);
     if (idx > _ledNum) return false;
     _ledBuffer[idx].r = r;
     _ledBuffer[idx].g = g;
     _ledBuffer[idx].b = b;
-    // SerialUSB.printf("BUF IDX: %2d, R: %3d, G: %3d, B: %3d\r\n", idx, _ledBuffer[idx].r, _ledBuffer[idx].g, _ledBuffer[idx].b);
     return true;
 }
 
@@ -49,25 +45,8 @@ void NeoPixel_SPI::show() {
     uint8_t* ledSendBuffer = (uint8_t*)malloc(sizeof(LedColor_t) * 4 * _ledNum);
 
     for (uint8_t i = 0; i < _ledNum; i++) {  // LED Count
-#if 0
-        SerialUSB.printf("BUF IDX: %2d, R: %3d, G: %3d, B: %3d\r\n", i, _ledBuffer[i].r, _ledBuffer[i].g, _ledBuffer[i].b); 
-        delay(1);
-#endif
         rgb_conv(ledSendBuffer + (i * 12), _ledBuffer[i].r, _ledBuffer[i].g, _ledBuffer[i].b);
     }
-#if 0
-    SerialUSB.printf("TEST\r\n"); 
-    for(uint8_t i = 0; i < _ledNum; i++) {
-        SerialUSB.printf("BUF[%2d]: %2x%2x%2x%2x, %2x%2x%2x%2x, %2x%2x%2x%2x\r\n", i, 
-            (uint8_t)*(ledSendBuffer + (i * 12) + 0), (uint8_t)*(ledSendBuffer + (i * 12) + 1), 
-            (uint8_t)*(ledSendBuffer + (i * 12) + 2), (uint8_t)*(ledSendBuffer + (i * 12) + 3), 
-            (uint8_t)*(ledSendBuffer + (i * 12) + 4), (uint8_t)*(ledSendBuffer + (i * 12) + 5), 
-            (uint8_t)*(ledSendBuffer + (i * 12) + 6), (uint8_t)*(ledSendBuffer + (i * 12) + 7), 
-            (uint8_t)*(ledSendBuffer + (i * 12) + 8), (uint8_t)*(ledSendBuffer + (i * 12) + 9), 
-            (uint8_t)*(ledSendBuffer + (i * 12) + 10), (uint8_t)*(ledSendBuffer + (i * 12) + 11)); 
-            delay(1); 
-    }
-#endif
     _spiLed->transfer(ledSendBuffer, sizeof(LedColor_t) * 4 * _ledNum, SPI_CONTINUE);
 
     free(ledSendBuffer);
